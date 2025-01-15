fcb30ef1cf4fb6833ac887be2149c73e300643e5
adamdavenportGov <143554568+adamdaventryGov@users.noreply.github.com>
2025-01-15 11:43:39 -0500

    disable redis in config file
    disable redis in config file and date

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ dotenv.config();
 
 const config = {
   version: process.env.VERSION,
-  date: process.env.DATE,
+  // date: process.env.DATE,
   BENTO_BACKEND_GRAPHQL_URI: process.env.BENTO_BACKEND_GRAPHQL_URI,
-  REDIS_HOST: process.env.REDIS_HOST,
-  REDIS_PORT: process.env.REDIS_PORT,
+  // REDIS_HOST: process.env.REDIS_HOST,
+  // REDIS_PORT: process.env.REDIS_PORT,
   AWS_REGION: process.env.AWS_REGION,
   S3_ACCESS_KEY_ID: process.env.S3_ACCESS_KEY_ID,
   S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
@@ -15,8 +15,8 @@ const config = {
   CLOUDFRONT_PRIVATE_KEY: process.env.CLOUDFRONT_PRIVATE_KEY,
   CLOUDFRONT_DOMAIN: process.env.CLOUDFRONT_DOMAIN,
   SIGNED_URL_EXPIRY_SECONDS: process.env.SIGNED_URL_EXPIRY_SECONDS,
-  REDIS_AUTH_ENABLED: process.env.REDIS_AUTH_ENABLED,
-  REDIS_PASSWORD: process.env.REDIS_PASSWORD,
+  // REDIS_AUTH_ENABLED: process.env.REDIS_AUTH_ENABLED,
+  // REDIS_PASSWORD: process.env.REDIS_PASSWORD,
 };
 
 function scanConfigObject(configObject) {
